581e28b83f587e5fb84b3fd6d6299504566a46e0
FaustinaG <faustina.g@psiog.com>
2020-02-06 18:51:53 +0530

    Changes 06/02

---
+++ BookingFlight/Controllers/FlightController.cs
@@ -0,0 +1,77 @@
+﻿using BookingFlight.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BookingFlight.Controllers
+{
+    public class FlightController : ApiController
+    {
+        public IHttpActionResult PostFlight(Flight flight)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+            using (var ctx = new BookingFlightEntities())
+            {
+                var flights = new Flight()
+                {
+                    FlightName = flight.FlightName,
+                    TotalSeats = flight.TotalSeats
+                };
+                ctx.Flights.Add(flights);
+
+                ctx.SaveChanges();
+                return Json(new { id = flights.Id });
+            }
+        }
+
+        public IHttpActionResult PutFlight(Flight flight)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+            using (var ctx = new BookingFlightEntities())
+            {
+                var flighttobeUpdated = ctx.Flights.Where(x => x.Id == flight.Id).FirstOrDefault<Flight>();
+                if(flighttobeUpdated != null)
+                {
+                    flighttobeUpdated.FlightName = flight.FlightName;
+                    flighttobeUpdated.TotalSeats = flight.TotalSeats;
+                }
+
+                ctx.SaveChanges();
+                
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult GetFlights()
+        {
+            IList<FlightViewModel> flights = null;
+
+            using (var ctx = new BookingFlightEntities())
+            {
+                flights = (from flight in ctx.Flights
+                           join flightDetail in ctx.FlightDetails
+                           on flight.Id equals flightDetail.FlightId
+                           select new FlightViewModel
+                           {    
+                               FlightName = flight.FlightName,
+                               TotalSeats = flight.TotalSeats,
+                               Id = flight.Id,
+                               FlightId = flight.Id,
+                               FlightIdTobeCanceled = flight.Id
+                           }).ToList<FlightViewModel>();
+            }
+
+            if (!flights.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(flights);
+        }
+    }
+}
--- BookingFlight/Controllers/FlightDetailController.cs
+++ BookingFlight/Controllers/FlightDetailController.cs
@@ -72,5 +72,61 @@ namespace BookingFlight.Controllers
 
             return Ok(flights);
         }
+
+        public IHttpActionResult GetFlightDetailsByFlightId(int id)
+        {
+            IList<FlightDetailViewModel> flights = null;
+
+            using (var ctx = new BookingFlightEntities())
+            {
+                flights = (from flight in ctx.Flights
+                           join flightDetail in ctx.FlightDetails
+                           on flight.Id equals flightDetail.FlightId
+                           where flightDetail.FlightId == id
+                           select new FlightDetailViewModel
+                           {
+                               Id = flightDetail.Id,
+                               FlightName = flight.FlightName,
+                               Departure = flightDetail.Departure,
+                               Arrival = flightDetail.Arrival,
+                               JourneyDate = flightDetail.Departure,
+                               FromCity = flightDetail.FromCity,
+                               ToCity = flightDetail.ToCity,
+                               Price = flightDetail.Price,
+                               SeatAvailability = flightDetail.SeatAvailability,
+                           }).ToList<FlightDetailViewModel>();
+            }
+
+            if (!flights.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(flights);
+        }
+
+        public IHttpActionResult PostFlightDetail(FlightDetail flight)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+            using (var ctx = new BookingFlightEntities())
+            {
+                var flights = new FlightDetail()
+                {
+                    FromCity = flight.FromCity,
+                    ToCity = flight.ToCity,
+                    Departure = flight.Departure,
+                    Arrival = flight.Arrival,
+                    Price = flight.Price,
+                    SeatAvailability = flight.SeatAvailability,
+                    FlightId = flight.FlightId
+                };
+                ctx.FlightDetails.Add(flights);
+
+                ctx.SaveChanges();
+                
+            }
+            return Ok();
+        }
     }
 }
